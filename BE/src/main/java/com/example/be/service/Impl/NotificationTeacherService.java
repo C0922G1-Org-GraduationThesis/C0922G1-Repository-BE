6b9023749ce107d365132625c831c24d9256c69a
unknown <nhattuanqlmt2@gmail.com>
2023-03-30 15:45:50 +0700

    first commit

---
+++ BE/src/main/java/com/example/be/service/Impl/NotificationTeacherService.java
@@ -0,0 +1,8 @@
+package com.example.be.service.Impl;
+
+import com.example.be.service.INotificationTeacherService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationTeacherService implements INotificationTeacherService {
+}
