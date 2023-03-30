55a637036ea051450674e2e1fb49f55b6e32c1d2
HoangJerry304 <hoangninh3004@gmail.com>
2023-03-30 10:20:23 +0700

    add branch teacher

--- BE/src/main/java/com/example/be/repository/IClazzRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.be.repository;
-
-import com.example.be.model.Clazz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IClazzRepository extends JpaRepository<Clazz, Long> {
-}
--- BE/src/main/java/com/example/be/service/Impl/AnnouncementService.java
+++
@@ -1,8 +0,0 @@
-package com.example.be.service.Impl;
-
-import com.example.be.service.IAnnouncementService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AnnouncementService implements IAnnouncementService {
-}
