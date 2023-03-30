01d2c4439a4dee9b47bb667bb2bb8cc5adddb896
HoangJerry304 <hoangninh3004@gmail.com>
2023-03-30 13:22:45 +0700

    add new source code for function JUnit test 5-6

--- BE/src/test/java/com/example/be/controller/NotificationTeacherRestController_getAllNotificationTeacher.java
+++ BE/src/test/java/com/example/be/controller/NotificationTeacherRestController_getAllNotificationTeacher.java
@@ -7,47 +7,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ProgressRestController_getListProgressProject {
+public class NotificationTeacherRestController_getAllNotificationTeacher {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void getListProgressProject_5() throws Exception {
+    public void getAllNotificationTeacher_5() throws Exception {
+
         this.mockMvc.perform(
                         MockMvcRequestBuilders
-                                .get("/api/progress/lit","")).andDo(print())
+                                .get("/api/notification"))
+                .andDo(print())
                 .andExpect(status().is4xxClientError());
     }
 
-    /**
-     * Danh sach rong,..
-     * @throws Exception
-     */
     @Test
-    public void getListProgressProject_6() throws Exception {
+    public void getAllNotificationTeacher_6() throws Exception {
+
         this.mockMvc.perform(
                         MockMvcRequestBuilders
-                                .get("/api/progress/list?page=0","")).andDo(print())
+                                .get("/api/notification-teachers"))
+                .andDo(print())
                 .andExpect(status().is2xxSuccessful())
-                .andExpect(jsonPath("totalPages").value(1))
-                .andExpect(jsonPath("totalElements").value(1))
-                .andExpect(jsonPath("content[0].projectId").value(1))
-                .andExpect(jsonPath("content[0].teamName").value("FB09"))
-                .andExpect(jsonPath("content[0].projectName").value("Thiết kế ứng dụng đặt vé xem phim"))
-                .andExpect(jsonPath("content[0].memberTotal").value(4));
+                .andExpect(jsonPath("$[0].notificationTeacherId").value(1));
     }
-
-    /**
-     * Danh sach rong,..
-     * @throws Exception
-     */
-
-
 }
