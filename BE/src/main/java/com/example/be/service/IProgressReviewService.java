2cb8a68c19a32bdd3941935184349fdcef90bfe3
unknown <you@example.com>
2023-03-30 13:50:14 +0700

    first commit

---
+++ BE/src/main/java/com/example/be/service/IProgressReviewService.java
@@ -0,0 +1,11 @@
+package com.example.be.service;
+
+import com.example.be.model.ProgressReview;
+
+import java.util.List;
+
+public interface IProgressReviewService {
+    List<ProgressReview>findAll();
+    ProgressReview findById(Long progressReviewId);
+    ProgressReview save(ProgressReview progressReview);
+}
